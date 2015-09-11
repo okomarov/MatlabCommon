1d6c8a5b0ad11f103b850d4437c22db8768f577d
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-09-11 19:57:17 +0100

    iscommonshare accepts shrcd as data not loading

--- common/iscommonshare.m
+++ common/iscommonshare.m
@@ -1,12 +1,13 @@
-function tf = iscommonshare(tb)
+function tf = iscommonshare(tb, shrcd)
 % ISCOMMONSHARE Checks which Id - Date pairs are common shares (share type code 10 and 11) 
 %
 %   ISCOMMONSHARE(TB) TB is a table with Id and yyyymmdd Date 
 if isa(tb,'dataset')
     tb = dataset2table(tb);
 end
-
-shrcd = loadresults('shrcd');
+if nargin < 2
+    shrcd = loadresults('shrcd');
+end
 shrcd = shrcd(shrcd.Shrcd == 11 | shrcd.Shrcd == 10,{'Permno','Date'});
 tf    = ismembIdDate(tb.Permno, tb.Date, shrcd.Permno, shrcd.Date);
 
