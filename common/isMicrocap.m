f5fd1ed2d1da86baee0968a836eeb2171b0e22dd
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-10-22 14:04:29 +0100

    fix isMicroCap with new getMktCap

--- common/isMicrocap.m
+++ common/isMicrocap.m
@@ -1,4 +1,4 @@
-function tf = isMicrocap(tb,lag,crsp,cap)
+function tf = isMicrocap(tb,fname,lag)
 % ISMICROCAP Checks which Id - Date pairs are microcaps
 %
 %   ISMICROCAP(TB) TB is a table with Id, Date (yyyymmdd) and Price 
@@ -6,24 +6,15 @@ function tf = isMicrocap(tb,lag,crsp,cap)
 % NOTE: it is true by default for first LAG observations of each ID
 %       
 
-if nargin < 2, lag = 1; end
+if nargin < 3, lag = 1; end
 
-% CRSP close prices
-if nargin < 3
-    try
-        crsp = loadresults('dsfquery');
-    catch
-        crsp = loadresults('dsfquery','..\results');
-    end
-end
-
-% CRSP market capitalizations (no. of shares are expressed in thousands)
-if nargin < 4
-    try
-        cap = loadresults('mktcap');
-    catch
-        cap = loadresults('mktcap','..\results');
-    end
+% Ensure it is sorted by Permno and Date
+idx = diff(tb.Date) == 0;
+if ~all(tb.Permno(idx) ~= tb.Permno([false;idx]))
+    [tb,isort] = sortrows(tb,{'Permno','Date'});
+    SORT_BACK  = true;
+else
+    SORT_BACK = false;
 end
 
 % Nyse breakpoints (no. of shares are expressed in millions)
@@ -35,25 +26,26 @@ end
 bpoints      = bpoints(ismember(bpoints.Date, unique(tb.Date/100)),{'Date','Var3'});
 bpoints.Var3 = [NaN(lag,1); bpoints.Var3(lag+1:end)];
 
-% Sort
-[tb, isort] = sortrows(tb,{'Permno','Date'});
+% price filter
+tb.IsPriceBelow = [false(lag,1); tb.(fname)(1+lag:end) < 5];
+
+% Get lagged market cap
+cap           = getMktCap(tb,lag,false);
+[idx,pos]     = ismembIdDate(tb.Permno, tb.Date, cap.Permno,cap.Date);
+tb.Cap(idx,1) = cap.Cap(pos(idx));
+% Market cap filter versus 1st Nyse decile
+[~,pos]       = ismember(tb.Date/100,bpoints.Date);
+tb.NyseCap    = bpoints.Var3(pos)*1000;
+tb.IsCapBelow = tb.Cap < tb.NyseCap;
+
+% Output
+tf = tb.IsPriceBelow | tb.IsCapBelow;
 
 % Permno as grouping label to account for lag
-igroup = [false(lag,1); tb.Permno(1:end-lag) == tb.Permno(1+lag:end)];
-
-% price filter
-[idx,pos] = ismembIdDate(tb.Permno, tb.Date, crsp.Permno,crsp.Date);
-price     = crsp.Prc(pos(idx));
-iprice    = [false(lag,1); price(1+lag:end) < 5];
-
-% Mkt cap filter
-[~,pos]   = ismember(tb.Date/100,bpoints.Date);
-nyseCap   = bpoints.Var3(pos)*1000;
-[idx,pos] = ismembIdDate(tb.Permno, tb.Date, cap.Permno,cap.Date);
-cap       = cap.Cap(pos(idx));
-icap      = [inf(lag,1); cap(1+lag:end)]  < nyseCap;
-
-tf          = iprice | icap;
+igroup      = [false(lag,1); tb.Permno(1:end-lag) == tb.Permno(1+lag:end)];
 tf(~igroup) = true;
-tf(isort)   = tf;
+
+if SORT_BACK
+    tf(isort) = tf;
+end
 end
