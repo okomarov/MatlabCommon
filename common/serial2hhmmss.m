e16aaa0a4607b887da88e501e4df08c6bead4746
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-10-30 16:25:31 +0000

    speedup serial2hhmmss

--- common/serial2hhmmss.m
+++ common/serial2hhmmss.m
@@ -2,6 +2,6 @@ function x = serial2hhmmss(n)
 % SERIAL2HHMMSS Convert serial time to HHMMSS format
 %
 % NOTE: discards date
-x = datevec(n);
+x = datevec(mod(n,1));
 x = x(:,4:end)*[10000;100;1];
 end
