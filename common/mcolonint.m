f7d9088eb413e0ef041389095cbd8d9aad8aae6e
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-11-03 23:21:55 +0000

    allow nonsorted from/to

--- common/mcolonint.m
+++ common/mcolonint.m
@@ -3,11 +3,16 @@ function idx = mcolonint(from, to)
 %
 % NOTE: only supports whole from/to and the step size is 1.
 
+oldclass = class(to);
+from     = int64(from);
+to       = int64(to);
+
 nobs     = to-from+1;
 idx      = ones(sum(nobs),1);
-pos      = cumsum(nobs(1:end-1))+1;
-df       = from(2:end) - to(1:end-1);
+pos      = [1; cumsum(nobs(1:end-1))+1];
+df       = [from(1); from(2:end) - to(1:end-1)];
 idx(pos) = df;
-idx(1)   = from(1);
 idx      = cumsum(idx);
+
+idx = cast(idx,oldclass);
 end
