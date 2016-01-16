4afa329d8d997876a6dc5ec563e39ce8ea4a61a4
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-01-16 20:48:51 +0000

    some checks on inputs

--- common/in.m
+++ common/in.m
@@ -1,9 +1,14 @@
 function idx = in(A, bounds, inclusion)
 % idx = in(A, bounds, inclusion)
+if numel(bounds) < 2
+    error('in:numBounds','BOUNDS should have two dates, i.e [from, to].')
+end
 
 bounds = sort(bounds,2);
 if nargin < 3 || isempty(inclusion)
     inclusion = '[]';
+elseif ~ischar(inclusion)
+    error('in:inclusionType','INCLUSION should be char.')
 end
 
 lb = bounds(:,1);
