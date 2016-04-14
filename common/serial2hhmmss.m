50303e0476dcbd5da037569c0c30c89e82b90f33
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-04-14 17:04:49 +0100

    keep input shape

--- common/serial2hhmmss.m
+++ common/serial2hhmmss.m
@@ -2,6 +2,12 @@ function x = serial2hhmmss(n)
 % SERIAL2HHMMSS Convert serial time to HHMMSS format
 %
 % NOTE: discards date
-x = datevec(mod(n,1));
-x = x(:,4:end)*[10000;100;1];
+
+sz = size(n);
+x  = datevec(mod(n,1));
+x  = x(:,4:end)*[10000;100;1];
+
+if sz(2) > 1
+    x = reshape(x,sz);
+end
 end
