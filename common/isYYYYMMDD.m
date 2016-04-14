6c48d2d9f7c5da5fe984824010d0187c73e55d57
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-04-14 21:25:54 +0100

    vector-like result

--- common/isYYYYMMDD.m
+++ common/isYYYYMMDD.m
@@ -1,5 +1,8 @@
 function bool = isYYYYMMDD(date)
 % ISYYYYMMDD Checks that date is in numeric YYYYMMDD format
-
-bool = isnumeric(date) && all(mod(date,1) == 0) && all(in(date, [0,99999999]));
+if isnumeric(date)
+    bool = mod(date,1) == 0 & in(date, [0,99999999]);
+else
+    bool = false;
+end
 end
