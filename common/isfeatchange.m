f373e460302e45a2b2c228afcb0f6211fcafd6e3
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-04-25 18:18:28 +0100

    handle single line case

--- common/isfeatchange.m
+++ common/isfeatchange.m
@@ -2,31 +2,39 @@ function idx = isfeatchange(tb, inum)
 
 % ISFEATCHANGE Progressively detects changes across features/ids
 %
-%   ... = ISFEATCHANGE(TB) TB should be a table with: 
-% 
+%   ... = ISFEATCHANGE(TB) TB should be a table with:
+%
 %               ID | feature cols | DATES
-%          
+%
 %         ISFEATCHANGE(..., INUM) INUM is a logical vector telling
 %                                 which columns are numeric. By default
-%                                 INUM is all true. 
-%                                 Non numeric columns are considered 
+%                                 INUM is all true.
+%                                 Non numeric columns are considered
 %                                 cellstrings.
 %
-%   IDX = ... 
+%   IDX = ...
 %         Returns the index to the records to keep
 %
 %
 %   NOTE: this function is used to retain the earliest-in-time records when
 %         a feture changed. It is designed to avoid accumarray() in the
 %         following situation:
-%           
-%         [Input]              [accumarray(...,@min(date))]            
 %
-%         feat         date    
+%         [Input]              [accumarray(...,@min(date))]
+%
+%         feat         date
 %            2   2012/12/22     1
 %            2   2012/12/31     0
 %            1   2013/01/05     1
 %            2   2013/02/07     0 <- feature change not picked!
+
+sz = size(tb);
+
+% Single line
+if sz(1) == 1
+    idx = true;
+    return
+end
 
 % Properly sorted
 [~,~,subs] = unique(tb(:,[1,end]));
@@ -34,18 +42,17 @@ if ~issorted(subs)
     error('tconsolidate:unsorted', 'TB should be sorted by Id and Date.')
 end
 % Inum
-szTb = size(tb);
 if nargin < 2 || isempty(inum)
-    inum = 1:szTb(2);
-elseif islogical(inum) 
+    inum = 1:sz(2);
+elseif islogical(inum)
     inum = find(inum);
 elseif isequal(unique(inum),[0,1])
     inum = find(inum);
 end
 
 vnames = getVariableNames(tb);
-idx = false(size(tb,1)-1,1);
-for c = 1:szTb(2)-1
+idx    = false(size(tb,1)-1,1);
+for c = 1:sz(2)-1
     field = vnames{c};
     v     = tb.(field);
     if any(c == inum)
