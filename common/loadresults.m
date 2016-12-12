b6f99967c3051aad2262a9bc9a365595d0dc2c52
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-12-12 20:35:45 +0000

    improve file matching

--- common/loadresults.m
+++ common/loadresults.m
@@ -4,21 +4,27 @@ function out = loadresults(name, resdir)
 %   LOADRESULTS(NAME) Loads the latest '*NAME.mat'
 
 if nargin < 2
-    resdir  = '.\results';
+    resdir = '.\results';
 end
 
 if isempty(strfind(name,'.mat'))
     name = [name, '.mat'];
 end
 % Most recent
-files   = dir(fullfile(resdir, ['*', name]));
+files = dir(fullfile(resdir, ['*', name]));
 if isempty(files)
     error('loadresults:nofile','No files matching ''%s'' found.',name)
 end
-[~,idx] = max([files.datenum]); 
-s       = load(fullfile(resdir,files(idx).name));
+
+% Prefer exact match or take most recent
+idx = ismember({files.name},name);
+if ~any(idx)
+    [~,idx] = max([files.datenum]);
+end
+s = load(fullfile(resdir,files(idx).name));
+
 % Load whole structure if multiple fields
-fnames  = fieldnames(s);
+fnames = fieldnames(s);
 if numel(fnames) == 1
     out = s.(fnames{1});
 else
